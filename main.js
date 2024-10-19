1413fa2d9b1b9bcf7f4719cf9afbc4d3911c6fec
DungNguyen <dung141220072@gmail.com>
2024-10-19 17:04:31 +0700

    init file

---
+++ main.js
@@ -0,0 +1,43 @@
+// Thay đổi nội dung búc thư ở đây
+var letterContent =" Nhân ngày 20/10 Dũng và Nguyên xin chúc tất cả con gái trong CLB Rise And Shine luôn hạnh phúc,vui vẻ,thành công và đạt được nhiều thành tựu trong công việc và học tập đồng thời sẽ gắn bó với CLB càng lâu càng tốt"
+// Tốc độ viết chữ. Số càng nhỏ tốc độ càng nhanh. 50 là tốc độ khá phù hợp
+durationWrite = 50 
+
+// Hiệu ứng gõ chữ
+
+function effectWrite () {
+    var boxLetter = document.querySelector(".letterContent")
+    letterContentSplited = letterContent.split("")
+    
+    letterContentSplited.forEach((val, index) => {
+        setTimeout(() => {
+            boxLetter.innerHTML += val    
+        }, durationWrite* index)
+    })
+}
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.querySelector(".container").classList.add("active")
+    }, 500)
+})
+
+var openBtn = document.querySelector(".openBtn")
+openBtn.addEventListener("click", () => {
+    document.querySelector(".cardValentine").classList.add("active")
+    document.querySelector(".container").classList.add("close")
+})
+
+var cardValentine = document.querySelector(".cardValentine")
+
+cardValentine.addEventListener("click", () => {
+    cardValentine.classList.toggle("open")
+
+    if(cardValentine.className.indexOf("open") != -1) {
+        setTimeout(effectWrite, 500)
+    } else {
+        setTimeout(() => {
+            document.querySelector(".letterContent").innerHTML = ""
+        }, 1000)
+    }
+})
